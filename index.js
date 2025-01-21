8bac6a452510626e20598d2a1b15ae37a02442e9
CompSciHigh24 <csh-github@compscihigh.org>
2025-01-21 01:52:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,27 @@
 const express = require("express");
 const app = express();
 
-// Task 0: Create the public and views folders and move the correct files into place
-// Task 1: Set up the static files middleware
+// Task 1: Create the public and views folders and move the correct files into place
 
 
-// Task 2: Set up a route handler for / that sends back index.html
-
-
+// Task 2: Set up the static file middleware
 
 
 // Task 3: Set the view engine to EJS. 
 
 
-// Task 4: Set up a route handler for /student/1 that...
+// Task 4: Set up a route handler for / that...
 
-  // ...creates an object called student with the following properties
-  // "name" (string)
-  // "advisory" (string)
-  // "imageUrl" (string)
-  // "funFact" (string)
+  // ...creates an object called post with the following properties
+  // "username" (string)
+  // "comment" (string)
+  // "pfpURL" (string)
+  // "postURL" (string)
   
 
-  // ...and sends back student.ejs with the data object
+  // ...and renders post.ejs with this data object
 
-
-// Task 5: Set up a route handler for /student/2 that serves a DIFFERENT student using the same template
-
-
-// EXTRA CREDIT
-// Task 6: Try to write your own teacher.ejs template that sends back a slightly different info for a teacher (e.g. classes, department)
-// Task 7: Set up a route handlers for /teacher/1 and /teacher/2 that use your new template!
-
-
+// Task 5: CONTINUED IN POST.EJS
 
 
 app.listen(3000, ()=>{
